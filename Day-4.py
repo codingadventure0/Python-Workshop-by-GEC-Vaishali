2d5010d481a4c72726e3d908782dc351c36515a3
Abhishek Kumar <136961510+codingadventure0@users.noreply.github.com>
2024-09-26 00:16:40 +0530

    First Python Code Uploading

---
+++ Day-4.py
@@ -0,0 +1,55 @@
+print("Hello, Python!")
+
+#Variables and Assignment 
+x = 101
+name = "Anonymous"
+print(x, name)
+
+# This is a single-line comment
+
+'''
+This is a multi-line comment
+'''
+
+a = 5
+b = 3
+print(a + b)  
+print(a - b)  
+print(a * b)  
+print(a / b)  
+
+#Variables and Data Types
+integer_var = 101
+float_var = 101.1
+string_var = "Anonymous"
+bool_var = True
+
+# Type Checking 
+print(type(integer_var))  
+print(type(float_var))
+print(type(string_var))
+print(type(bool_var))
+
+
+str1 = "Hello"
+str2 = "Python"
+print(str1 + " " + str2)  
+
+# String Indexing
+s = "Python"
+print(s[0])  
+print(s[-1])
+
+# Control Statements
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+
+y = 101
+if y > 15:
+    print("Greater than 15")
+elif y > 5:
+    print("Greater than 5")
+else:
+    print("Less than or equal to 5")
