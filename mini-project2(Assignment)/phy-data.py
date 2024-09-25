3a2eb36150f713b91363035a88869d1027fa6a35
abhibth977 <abhibth977@gmail.com>
2024-09-26 01:09:13 +0530

    solving Q.no: 2

--- mini-project2(Assignment)/phy-data.py
+++ mini-project2(Assignment)/phy-data.py
@@ -6,8 +6,13 @@ import matplotlib.pyplot as plt
 df=pd.read_csv("Physics.csv")
 
 # 1. Highest attendance student in first five rows
-x=df.head().iloc[:,1]
-y=df.head().iloc[:,3]
+# x=df.head().iloc[:,1]
+# y=df.head().iloc[:,3]
+# plt.bar(x,y)
+# plt.show()
+
+# 2. Highest attendance student in Last five rows
+x=df.tail().iloc[:,1]
+y=df.tail().iloc[:,3]
 plt.bar(x,y)
 plt.show()
-
