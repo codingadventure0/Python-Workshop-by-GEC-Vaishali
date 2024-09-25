12598306ff9db249a95d8ff012b43f1b515a6561
abhibth977 <abhibth977@gmail.com>
2024-09-26 01:08:21 +0530

    solving Q.no: 1

---
+++ mini-project2(Assignment)/phy-data.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df=pd.read_csv("Physics.csv")
+
+# 1. Highest attendance student in first five rows
+x=df.head().iloc[:,1]
+y=df.head().iloc[:,3]
+plt.bar(x,y)
+plt.show()
+
