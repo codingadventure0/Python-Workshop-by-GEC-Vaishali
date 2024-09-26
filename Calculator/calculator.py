a277ce021fa415431cb209e17c99d801564569bb
abhibth977 <abhibth977@gmail.com>
2024-09-27 00:22:14 +0530

    creating a simple gui calculator

---
+++ Calculator/calculator.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root=Tk()
+root.title("Simple Calculator")
+root.geometry("500x700")
+
+def calculate():
+    result = eval(entry.get())
+    result_label.config(text="Result: " + str(result))
+
+entry=Entry(root, width=20)
+entry.pack(pady=10)
+
+calc_button=Button(root, text="Calculate", command=calculate)
+calc_button.pack(pady=5)
+
+result_label=Label(root, text="Result: ")
+result_label.pack(pady=10)
+
+root.mainloop()
